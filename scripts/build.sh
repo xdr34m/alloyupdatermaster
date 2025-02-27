7bdaf60f5300e586b375d109199b800072e19a5d
Julian Kirstein <juliankirstein37@gmail.com>
2025-02-27 02:12:40 +0100

    changed dir structur

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,5 +6,5 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_ROOT="$(dirname "$SCRIPT_DIR")"
 cd "$PROJECT_ROOT/src"
 
-GOOS=windows GOARCH=amd64 CGO_ENABLED=0 go build -o ../builds/shellExecutor-windows-amd64.exe .
-GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o ../builds/shellExecutor-linux-amd64 .
+GOOS=windows GOARCH=amd64 CGO_ENABLED=0 go build -o ../builds/alloyupdater-windows-amd64.exe .
+GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o ../builds/alloyupdater-linux-amd64 .
